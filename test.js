15a20244a73670dae76958598c4433fd0c669f5d
sandatya <gungtia73@yahoo.com>
2023-09-05 23:12:48 +0800

    commit pertama

---
+++ test.js
@@ -0,0 +1,22 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+// TODO
+let firstName = "sandatya";
+let lastName = " widhiyanti";
+let age =24;
+let isMarried = false;
+console.log("Nama Lengkap : "+firstName+lastName);
+console.log("usia : "+age);
+console.log(isMarried);
+/**
+ * Hiraukan kode di bawah ini
+ */
+
+
